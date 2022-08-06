80740419a64a05c0610dfd4fdb3522879a2884a5
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-08-07 01:38:48 +0300

    task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateStringArr(int n)
+{
+    string[] newArray = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Input {i + 1} element: ");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+
+int countElement(string[] array)
+{
+    int elementCount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) elementCount++;
+
+    }
+    return elementCount;
+}
+
+string[] outArr(string[] arr, int count)
+{
+    string[] arrOut = new string[count];
+    int n = 0;
+    for (int i = 0; n < arr.Length; i++)
+    {
+        if (arr[i].Length <=3)
+            {
+                arrOut[n] = arr[i];
+                n++;
+            }
+    }
+    return arrOut;
+}
+
+void PrintArray(string[] array)
+{
+
+
+}
+
+
+int randomMin = 1;
+int randomMax = 9;
+int arrSize = new Random().Next(randomMin, randomMax+1);
+Console.WriteLine($"Заполните количество {inputArraySize} элементов массива");
+
+string[] inputArray = CreateStringArr(inputArray);
